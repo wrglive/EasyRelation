f866f09310c5d8bd7142c683a13855a1e06e0bcf
wangruigang <wangruigang@zhuanzhuan.com>
2019-08-18 21:16:23 +0800

    Initial commit

---
+++ src/main/java/com/marshall/sky/graph/SkyGraphApplication.java
@@ -0,0 +1,13 @@
+package com.marshall.sky.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyGraphApplication.class, args);
+	}
+
+}
